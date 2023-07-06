bb3cbbdbf6550d57398ac5f8d377fcbb02146c48
whtnqls124578 <whtnqls124578@naver.com>
2023-07-06 23:46:24 +0900

    refactor : project 페이지에서 box skills 내용 상황에 맞게 바뀌지 않는동작 처리완료

--- portfolio/src/components/project/Right/ProjectSkills.tsx
+++ portfolio/src/components/project/Right/ProjectSkills.tsx
@@ -12,14 +12,10 @@ interface propsSkills {
 
 const Skills: React.FC<propsSkills> = ({skills}) => {
 
-    const [skill, setSkill] = useState(skills);
-
-    console.log(skill); 
-
     return (
         <div className='project_div-right-skill'>
             {/* Object.entries를 사용하여 객체의 속성과 값을 쌍으로 가지는 배열로 변환 */}
-            {Object.entries(skill).map(([key, value]) => {
+            {Object.entries(skills).map(([key, value]) => {
                 return (
                     <div className='project_div-right-skillBtn' key={key}>
                         <span>{value}</span>
