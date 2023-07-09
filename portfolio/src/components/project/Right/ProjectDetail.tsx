2a8eccf84f8a006700b2887060b1f480c5d86a65
whtnqls124578 <whtnqls124578@naver.com>
2023-07-10 03:30:37 +0900

    create : Project 페이지 스크롤하면 다음 subTitle 내용으로 넘어가기 완료

--- portfolio/src/components/project/Right/ProjectDetail.tsx
+++ portfolio/src/components/project/Right/ProjectDetail.tsx
@@ -4,11 +4,11 @@ import { Projects } from '../../../data/Project';
 // Pick 유틸리티 타입을 사용하여 Projects 인터페이스에서 text 속성만 선택
 const ProjectDetail: React.FC<Pick<Projects, 'text'>> = ({ text }) => {
 
-    console.log('text', text);
+    // console.log('text', text);
 
     return (
         <div className='project_div-right-detail'>
-            {text.map((item,idx)=>{
+            {text?.map((item,idx)=>{
                 return(
                     // React.Fragment : 추가적인 DOM 노드를 생성하지 않고 여러 개의 자식 요소를 그룹화하는 역할
                     <React.Fragment>
